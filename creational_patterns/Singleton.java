7ec62299c54c94d69bb54c12b07630b4329abbd3
ryjtoh <60204869+ryjtoh@users.noreply.github.com>
2024-07-28 23:46:47 -0700

    singleton WIP

---
+++ creational_patterns/Singleton.java
@@ -0,0 +1,19 @@
+package creational_patterns;
+/*
+Singleton definition:
+    - Ensures that a class only has one instance that is globally accessible.
+
+Why a Singleton?
+    - To control access to a shared resource such as a database or a file.
+    - To provide global access to this instance of a class.
+    - To prevent the variable from being overwritten.
+
+How to implement a Singleton:
+    - Make the default constructor private.
+    - Creation of the instance should be done statically.
+
+ */
+
+
+public class Singleton {
+}
