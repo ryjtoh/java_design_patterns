2640368c323c665312a7edf5698e568afc739698
ryjtoh <60204869+ryjtoh@users.noreply.github.com>
2024-08-12 09:38:47 -0700

    finished singleton

--- creational_patterns/Singleton.java
+++ creational_patterns/Singleton.java
@@ -10,10 +10,28 @@ Why a Singleton?
 
 How to implement a Singleton:
     - Make the default constructor private.
-    - Creation of the instance should be done statically.
+    - Creation/retrieval of the instance should be done in a static method
 
  */
 
 
 public class Singleton {
+    private static Singleton instance;
+    public String value;
+
+    private Singleton(String value) {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+        this.value = value;
+    }
+
+    public static Singleton getInstance(String value) {
+        if (instance == null) {
+            instance = new Singleton(value);
+        }
+        return Singleton.instance;
+    }
 }
